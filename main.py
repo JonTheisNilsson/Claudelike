98b6520cb30247e5a63e7caf6074f45bad0884fa
Jon Theis Nilsson <ignorante85@gmail.com>
2024-06-28 01:23:36 +0200

    refactor main.py

--- main.py
+++ main.py
@@ -61,6 +61,24 @@ def spawn_items(game_map, num_items=5):
             item = Armor("Chainmail", "Protective chainmail armor.", defense_bonus=random.randint(1, 3))
         game_map.items.append((x, y, item))
 
+def handle_player_movement(player, current_map, keys, current_time, last_move_time):
+    if current_time - last_move_time < MOVE_DELAY:
+        return False, last_move_time
+
+    if keys[WAIT]:
+        current_map.update_npcs()
+        return True, current_time
+
+    for key, direction in MOVE_DIRECTIONS.items():
+        if keys[key]:
+            old_pos = player.pos.copy()
+            player.move(*direction, current_map)
+            current_map.update_npcs()
+            if player.pos != old_pos:
+                return True, current_time
+
+    return False, last_move_time
+
 def handle_combat(player, game_map):
     for npc in game_map.npcs:
         if abs(npc.x - player.pos[0]) <= 1 and abs(npc.y - player.pos[1]) <= 1:
@@ -79,12 +97,72 @@ def handle_combat(player, game_map):
                     return False
     return True
 
+def handle_stairs(player, current_map):
+    if current_map.is_stairs(player.pos[0], player.pos[1]):
+        stairs_char = current_map.get_stairs_char(player.pos[0], player.pos[1])
+        if stairs_char == '<':
+            add_message("You climb up the stairs...")
+            return change_map(player, current_map, "up")
+        elif stairs_char == '>':
+            add_message("You descend down the stairs...")
+            return change_map(player, current_map, "down")
+    return current_map
+
+def handle_item_pickup(player, current_map):
+    for x, y, item in current_map.items:
+        if (x, y) == tuple(player.pos):
+            if player.add_to_inventory(item):
+                add_message(f"You picked up {item.name}!")
+                current_map.items.remove((x, y, item))
+            else:
+                add_message("Your inventory is full. You can't pick up the item.")
+            return
+
+def display_inventory(player):
+    add_message("Inventory:")
+    for i, item in enumerate(player.inventory):
+        add_message(f"{i + 1}. {item.name}")
+
+def game_loop(screen, clock, player, current_map):
+    last_move_time = 0
+
+    while True:
+        current_time = pygame.time.get_ticks()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.VIDEORESIZE:
+                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+        
+        keys = pygame.key.get_pressed()
+        
+        moved, last_move_time = handle_player_movement(player, current_map, keys, current_time, last_move_time)
+        
+        if moved:
+            current_map = handle_stairs(player, current_map)
+            handle_item_pickup(player, current_map)
+            
+            if not handle_combat(player, current_map):
+                add_message("Game Over! You have been defeated.")
+                pygame.time.wait(2000)  # Wait for 2 seconds before quitting
+                return False
+
+        if keys[pygame.K_i]:
+            display_inventory(player)
+        
+        camera_offset = get_camera_offset(player.pos, screen.get_size())
+        draw_game(screen, current_map, player, camera_offset)
+        pygame.display.flip()
+        clock.tick(60)
+
+    return True
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(INITIAL_WINDOW_SIZE, pygame.RESIZABLE)
     pygame.display.set_caption("Claudelike")
 
-    # Clear the map folder at the start of the game
     MapHandler.clear_map_folder()
 
     current_map = GameMap()
@@ -97,76 +175,11 @@ def main():
 
     clock = pygame.time.Clock()
     
-    last_move_time = 0
-    
-    while True:
-        current_time = pygame.time.get_ticks()
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.VIDEORESIZE:
-                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-        
-        # Handle movement and waiting
-        keys = pygame.key.get_pressed()
-        moved = False
-        
-        if current_time - last_move_time >= MOVE_DELAY:
-            if keys[WAIT]:
-                current_map.update_npcs()
-                moved = True
-                last_move_time = current_time
-            else:
-                for key, direction in MOVE_DIRECTIONS.items():
-                    if keys[key]:
-                        old_pos = player.pos.copy()
-                        player.move(*direction, current_map)
-                        current_map.update_npcs()
-                        if player.pos != old_pos:
-                            moved = True
-                            if current_map.is_stairs(player.pos[0], player.pos[1]):
-                                stairs_char = current_map.get_stairs_char(player.pos[0], player.pos[1])
-                                if stairs_char == '<':
-                                    add_message("You climb up the stairs...")
-                                    current_map = change_map(player, current_map, "up")
-                                elif stairs_char == '>':
-                                    add_message("You descend down the stairs...")
-                                    current_map = change_map(player, current_map, "down")
-                        
-                        last_move_time = current_time
-                        break
-
-        # Handle item pickup
-        if player.pos in [(x, y) for x, y, _ in current_map.items]:
-            item_index = [(x, y) for x, y, _ in current_map.items].index(tuple(player.pos))
-            _, _, item = current_map.items.pop(item_index)
-            if player.add_to_inventory(item):
-                add_message(f"You picked up {item.name}!")
-            else:
-                add_message("Your inventory is full. You can't pick up the item.")
-                current_map.items.append((player.pos[0], player.pos[1], item))
-
-        # Handle inventory display
-        if keys[pygame.K_i]:
-            add_message("Inventory:")
-            for i, item in enumerate(player.inventory):
-                add_message(f"{i + 1}. {item.name}")
-
-        if moved:
-            if not handle_combat(player, current_map):
-                add_message("Game Over! You have been defeated.")
-                pygame.time.wait(2000)  # Wait for 2 seconds before quitting
-                break
-        
-        camera_offset = get_camera_offset(player.pos, screen.get_size())
-        draw_game(screen, current_map, player, camera_offset)
-        pygame.display.flip()
-        clock.tick(60)
+    game_running = game_loop(screen, clock, player, current_map)
 
     pygame.quit()
-    sys.exit()
+    if not game_running:
+        sys.exit()
 
 if __name__ == "__main__":
     main()
